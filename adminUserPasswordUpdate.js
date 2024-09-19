70004dd600dbba9a9ffb47c7ecb03a31088bc228
Jim Zheng <z5394791@ad.unsw.edu.au>
2024-09-19 17:55:55 +1000

    removed two files.

--- adminUserPasswordUpdate.js
+++
@@ -1,4 +0,0 @@
-//Given details relating to a password change, this function updates the password of a logged in user.
-function adminUserPasswordUpdate (authUserId, oldPassword, newPassword) {
-    return {}; 
-}
