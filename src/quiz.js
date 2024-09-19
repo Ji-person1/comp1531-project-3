3483c9bc68473439f4104867acf814985c3b3289
Jim Zheng <z5394791@ad.unsw.edu.au>
2024-09-19 17:40:34 +1000

    Revert "Merge branch 'swastik/iteration0/StubFunctionComments' into 'master'"
    This reverts merge request !18

--- src/quiz.js
+++ src/quiz.js
@@ -1,10 +1,7 @@
-//Update the description of the relevant quiz.
-function adminQuizDescriptionUpdate (authUserId, quizId, name) { 
+function adminQuizDescriptionUpdate (authUserId, quizId, name) {
     return {}
 }
 
-
-//Get all of the relevant information about the current quiz.
 function adminQuizInfo(authUserId, quizId) {
     return {
         quizId: 1,
@@ -15,8 +12,7 @@ function adminQuizInfo(authUserId, quizId) {
     }
 }
 
-// Provides a list of all quizzes that are owned by the currently logged in user.
-function adminQuizList( authUserId ) 
+function adminQuizList( authUserId ) // Provides a list of all quizzes that are owned by the currently logged in user.
 {
     return { quizzes: [
         {
@@ -27,19 +23,17 @@ function adminQuizList( authUserId )
     }
 }
 
-//Given basic details about a new quiz, create one for the logged in user.
-function adminQuizCreate ( authUserId, name, description ) 
+function adminQuizCreate ( authUserId, name, description ) //Given basic details about a new quiz, create one for the logged in user.
 {
     return {
         quizId: 2
     }
 }
 
-//Given a particular quiz, permanently remove the quiz.
 function adminQuizRemove ( authUserId, quizId ) 
 {
     return {
-
+        
     }
 
 }
