d43d289e5230347e1527c1a612e9c2b1d51d4f00
Jim Zheng <z5394791@ad.unsw.edu.au>
2024-09-19 16:39:31 +1000

    Update adminAuthRegister with a comment

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,7 @@
+// Sample stub for the adminAuthRegister function
+// creates a user and returns an authUserId linked with a newly created user. 
 function adminAuthRegister (email, password, nameFirst, nameLast) {
     return {
-    authUserId: 1
+        authUserId: 1
     }
 }
