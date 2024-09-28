6b2ae3e9e3710dba781b29444501f31dcefb5ed0
Hao Wu <z5281040@ad.unsw.edu.au>
2024-09-28 21:47:35 +1000

    finished test of function 'adminQuizDescriptionUpdate'.-Hao

--- src/adminquizDescriptionUpdate_test.js
+++ src/adminquizDescriptionUpdate_test.js
@@ -7,20 +7,20 @@ describe(('test_adminQuizDescriptionUpdate'), () => {
     beforeEach(() => {
         clear();
         adminAuthRegister('sampleEmail@gmail.com', 'password123', 'nameFirst', 'nameLast');
-        adminQuizCreate(data.users[0].authUserId, data.users[0].name, 'description');
+        adminQuizCreate(data.users[0].authUserId, 'quizName1', 'description');
     });
     test(('AuthUserId is not a valid user'), () => {
-        let result = adminQuizDescriptionUpdate(data.users[0].authUserId + 1, data.quiezzes[0].quizId, 'description02');
+        let result = adminQuizDescriptionUpdate(data.users[0].authUserId + 1, data.quiezzes[0].quizId, 'description_new');
         expect(result).toStricEqual({error: 'specific error message here'});
     });
     test(('Quiz ID does not refer to a valid quiz'), () => {
-        let result = adminQuizDescriptionUpdate(data.users[0].authUserId, data.quiezzes[0].quizId + 1, 'description02');
+        let result = adminQuizDescriptionUpdate(data.users[0].authUserId, data.quiezzes[0].quizId + 1, 'description_new');
         expect(result).toStricEqual({error: 'specific error message here'});
     });
     test(('Quiz ID does not refer to a quiz that this user owns'), () => {
         adminAuthRegister('sampleEmail02@gmail.com', 'password123', 'firstName', 'lastName');
-        adminQuizCreate(data.users[1].authUserId, data.users[1].name, 'description');
-        let result = adminQuizDescriptionUpdate(data.users[1].authUserId, data.quiezzes[0].quizId, 'description02');
+        adminQuizCreate(data.users[1].authUserId, 'quizName2', 'description02');
+        let result = adminQuizDescriptionUpdate(data.users[1].authUserId, data.quiezzes[0].quizId, 'description02_new');
         expect(result).toStricEqual({error: 'specific error message here'});
     });
     test(('Description is more than 100 characters in length'), () => {
@@ -32,14 +32,14 @@ describe(('test_adminQuizDescriptionUpdate'), () => {
         expect(result).toStricEqual({error: 'specific error message here'});
     });
     test(('sucessfulTest1'), () => {
-        let result = adminQuizDescriptionUpdate(data.users[0].authUserId, data.quiezzes[0].quizId, 'description02');
-        expect(result).toStricEqual({});
+        let result = adminQuizDescriptionUpdate(data.users[0].authUserId, data.quiezzes[0].quizId, 'description_new');
+        expect(data.quiezzes[0].description).toStricEqual({description: 'description_new'});
     });
     test(('sucessfulTest2'), () => {
         adminAuthRegister('sampleEmail02@gmail.com', 'password123', 'firstName', 'lastName');
-        adminQuizCreate(data.users[1].authUserId, data.users[1].name, 'description');
-        let result = adminQuizDescriptionUpdate(data.users[1].authUserId, data.quiezzes[1].quizId, 'description02');
-        expect(result).toStricEqual({});
+        adminQuizCreate(data.users[1].authUserId, 'quizName2', 'description02');
+        let result = adminQuizDescriptionUpdate(data.users[1].authUserId, data.quiezzes[1].quizId, 'description02_new');
+        expect(data.quiezzes[1].description).toStricEqual({description: 'description02_new'});
     });
     clear();
 });
