c8b4c4226018bf0da636ff70617b067a5cb5fa8a
Hao Wu <z5281040@ad.unsw.edu.au>
2024-09-28 21:20:21 +1000

    complete function 'clear' and add 2 functions: 'isVaildUserId', 'isVaildQuizId'.-Hao

--- src/other.js
+++ src/other.js
@@ -1,3 +1,27 @@
-function clear(){
-    return{}
+import { data } from "./dataStore";
+
+function clear() {
+    data.users = [];
+    data.quiezzes = [];
+    return {}
 }
+
+function isVaildUserId(authUserId) {
+    for(let user of data.users) {
+        if (user.authUserId === authUserId) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isVaildQuizId(quizId) {
+    for(let quiz of data.quiezzes) {
+        if (quiz.quizId === quizId) {
+            return true;
+        }
+    }
+    return false;
+}
+
+export { clear, isVaildUserId, isVaildQuizId };
