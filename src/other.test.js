8589a6a2eb3c1816996866d0d4f175424517058d
Hao Wu <z5281040@ad.unsw.edu.au>
2024-09-29 17:10:40 +1000

    fix mistake of expected, and add one more test/-Hao

--- src/other.test.js
+++ src/other.test.js
@@ -1,23 +1,21 @@
 import { clear } from './other.js';
-import { data } from './dataStore.js';
 import { adminAuthRegister } from './auth.js';
 import { adminQuizCreate } from './quiz.js';
 
 describe(('2 tests function "clear"'), () => {
     test('empty_test', () => {
-        expect(clear()).toStrictEqual({
-            users : [],
-            quiezzes : []
-          });
+        expect(clear()).toStrictEqual({});
     });
     beforeEach(() => {
         adminAuthRegister('mail@gmail.com', 'this is password', 'first-name', 'last-name');
-        adminQuizCreate('z1111111', 'this is name', 'this is description');
+        adminQuizCreate(0, 'this is name', 'this is description');
     });
-    test('filled something and clean it', () => {
-        expect(clear()).toStrictEqual({
-            users : [],
-            quiezzes : []
-          });
+    test('fill something and clean it', () => {
+        expect(clear()).toStrictEqual({});
+    });
+    test('fill more things and clean it', () => {
+        adminAuthRegister('mail2@gmail.com', 'this is password', 'first-name2', 'last-name2');
+        adminQuizCreate(1, 'this is name', 'this is description');
+        expect(clear()).toStrictEqual({});
     });
 });
