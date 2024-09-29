0f94e38ece1489556cddadad755fbc1988f25778
Hao Wu <z5281040@ad.unsw.edu.au>
2024-09-29 17:11:32 +1000

    forget to save, add it again

--- src/other.test.js
+++ src/other.test.js
@@ -1,21 +1,22 @@
 import { clear } from './other.js';
+import { data } from './dataStore.js';
 import { adminAuthRegister } from './auth.js';
 import { adminQuizCreate } from './quiz.js';
 
 describe(('2 tests function "clear"'), () => {
     test('empty_test', () => {
-        expect(clear()).toStrictEqual({});
+        expect(clear()).toStrictEqual(data);
     });
     beforeEach(() => {
         adminAuthRegister('mail@gmail.com', 'this is password', 'first-name', 'last-name');
         adminQuizCreate(0, 'this is name', 'this is description');
     });
     test('fill something and clean it', () => {
-        expect(clear()).toStrictEqual({});
+        expect(clear()).toStrictEqual(data);
     });
     test('fill more things and clean it', () => {
         adminAuthRegister('mail2@gmail.com', 'this is password', 'first-name2', 'last-name2');
         adminQuizCreate(1, 'this is name', 'this is description');
-        expect(clear()).toStrictEqual({});
+        expect(clear()).toStrictEqual(data);
     });
 });
