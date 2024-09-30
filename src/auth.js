0e7f9303832d4a66eaf43261d4291422de0a24aa
Ji-person1 <139549779+Ji-person1@users.noreply.github.com>
2024-09-30 19:32:20 +1000

    Completed the tests for the function.

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,8 @@
+import validator from 'validator';
+
 // Sample stub for the adminAuthRegister function
 // creates a user and returns an authUserId linked with a newly created user. 
-function adminAuthRegister (email, password, nameFirst, nameLast) {
+export function adminAuthRegister (email, password, nameFirst, nameLast) {
     return {
         authUserId: 1
     }
