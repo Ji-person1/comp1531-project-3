829cdc1d5a5cdb0eafefe3bcba1abde3ba852293
Swastik Mishra <z5484885@ad.unsw.edu.au>
2024-10-03 17:35:24 +1000

    Committed a few files

---
+++ src/adminUserDetails.test.js
@@ -0,0 +1,43 @@
+import { adminAuthRegister, adminUserDetails } from './auth.js';
+import { clear } from './other.js';
+
+const ERROR = { error: expect.any(String) };
+
+beforeEach(() => {
+    clear()
+})
+
+describe('Error cases', () => {
+    test('empty data store', () => {
+        expect(adminUserDetails(1)).toEqual(ERROR);
+    });
+    test('invalid userId', () => {
+        const userId = adminAuthRegister("jim.zheng123@icloud.com", "1234abcd", "Jim", "Zheng")
+        expect(adminUserDetails(-userId)).toEqual(ERROR);
+    });
+});
+
+describe('Success cases', () => {
+    test('Sucessful view', () => {
+        const userId = adminAuthRegister("jim.zheng123@icloud.com", "1234abcd", "Jim", "Zheng")
+        expect(adminUserDetails(userId)).toEqual({user: {
+            userId: userId,
+            name: "Jim Zheng",
+            email: "jim.zheng123@icloud.com",
+            numSuccessfulLogins: 0,
+            numFailedPasswordsSinceLastLogin: 0
+        }});
+    });
+
+    test('Sucessful view wit multiple users', () => {
+        const userId = adminAuthRegister("jim.zheng123@icloud.com", "1234abcd", "Jim", "Zheng")
+        adminAuthRegister("z5394791@unsw.edu.au", "1234abcd", "Mij", "Zheng")
+        expect(adminUserDetails(userId)).toEqual({user: {
+            userId: userId,
+            name: "Jim Zheng",
+            email: "jim.zheng123@icloud.com",
+            numSuccessfulLogins: 0,
+            numFailedPasswordsSinceLastLogin: 0
+        }});
+    });
+});
--- src/quiz.js
+++ src/quiz.js
@@ -1,6 +1,35 @@
+import { getData } from "./dataStore";
 //Update the description of the relevant quiz.
+/**
+ * 
+ * @param {number} authUserId 
+ * @param {number} quizId 
+ * @param {string} description 
+ * @param {Array<number>} allMembers
+ * @returns {{}}
+ */
 function adminQuizDescriptionUpdate (authUserId, quizId, description) { 
-    return {}
+    const data = getData();
+    if (authUserId >= data.users.length) {
+        return { error: 'specific error message here' };
+    }
+    if (quizId >= data.quiezzes.length) {
+        return { error: 'specific error message here' };
+    }
+    const allMembers = data.quiezzes.allMembers;
+
+    if (description.length > 100) {
+        return { error: 'specific error message here' };
+    }
+
+    for (let i of allMembers) {
+        if (quizId == i) {
+            data.quiezzes.description = description;
+            return {};
+        }
+    }
+    
+    return { error: 'specific error message here' };
 }
 
 //Update the name of the relevant quiz.
@@ -21,16 +50,25 @@ function adminQuizInfo(authUserId, quizId) {
 }
 
 // Provides a list of all quizzes that are owned by the currently logged in user.
-function adminQuizList( authUserId ) 
-{
-    return { quizzes: [
-        {
-        quizId: 1,
-        name: 'My Quiz',
-        }
-    ]
+export function adminQuizList(authUserId) {
+    const data = getData();
+    const user = data.users.find(user => user.id === authUserId);
+
+
+    if (!user) {
+        return { error: 'Invalid authUserId' };
     }
+
+
+    const userQuizzes = data.quizzes.filter(quiz => quiz.creatorId === authUserId);
+    return {
+        quizzes: userQuizzes.map(quiz => ({
+            quizId: quiz.quizId,
+            name: quiz.name
+        }))
+    };
 }
+
 
 //Given basic details about a new quiz, create one for the logged in user.
 function adminQuizCreate ( authUserId, name, description ) 
