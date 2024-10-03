262de00d314bbc378292bcd408011cfbdba68fcb
swapnav saikia <swapnav@LAPTOP-RO9GM7AF>
2024-10-03 18:40:29 +1000

    completed tests for adminQuizInfo function

--- src/adminQuizInfo.test.js
+++ src/adminQuizInfo.test.js
@@ -19,7 +19,7 @@ describe('Error cases', () => {
         expect(adminQuizInfo(1)).toEqual(ERROR);
     });
     test ('invalid quizId', () => {
-        const userId = adminAuthRegister("swapnav.saikia@gmail.com", "1234abcd", "Swapnav", "Saikia")
+        const userId = adminAuthRegister("swapnav.saikia@icloud.com", "1234abcd", "Swapnav", "Saikia")
         const quizId = adminQuizCreate(userId, "quiz1", "This is my quiz")
         expect(adminQuizInfo(userId, -quizId)).toEqual(ERROR);
     });
