92f2f9dd8e6ecdb18c410fde871e0dfd03fde2e6
Hao Wu <z5281040@ad.unsw.edu.au>
2024-10-03 16:30:39 +1000

    change import of 'data' to 'getData'.-Hao

--- src/adminquizDescriptionUpdate_test.js
+++ src/adminquizDescriptionUpdate_test.js
@@ -1,27 +1,28 @@
 import { adminQuizDescriptionUpdate, adminQuizCreate } from './quiz.js';
 import { clear } from './other.js';
 import { adminAuthRegister } from './auth.js';
-import { data } from './dataStore.js';
+import { getdata } from './dataStore.js';
 
 describe(('test_adminQuizDescriptionUpdate'), () => {
     beforeEach(() => {
         clear();
+        const data = getdata();
         adminAuthRegister('sampleEmail@gmail.com', 'password123', 'nameFirst', 'nameLast');
         adminQuizCreate(data.users[0].authUserId, 'quizName1', 'description');
     });
     test(('AuthUserId is not a valid user'), () => {
         let result = adminQuizDescriptionUpdate(data.users[0].authUserId + 1, data.quiezzes[0].quizId, 'description_new');
-        expect(result).toStricEqual({error: 'specific error message here'});
+        expect(result).toStricEqual({error: expect.any(String)});
     });
     test(('Quiz ID does not refer to a valid quiz'), () => {
         let result = adminQuizDescriptionUpdate(data.users[0].authUserId, data.quiezzes[0].quizId + 1, 'description_new');
-        expect(result).toStricEqual({error: 'specific error message here'});
+        expect(result).toStricEqual({error: expect.any(String)});
     });
     test(('Quiz ID does not refer to a quiz that this user owns'), () => {
         adminAuthRegister('sampleEmail02@gmail.com', 'password123', 'firstName', 'lastName');
         adminQuizCreate(data.users[1].authUserId, 'quizName2', 'description02');
         let result = adminQuizDescriptionUpdate(data.users[1].authUserId, data.quiezzes[0].quizId, 'description02_new');
-        expect(result).toStricEqual({error: 'specific error message here'});
+        expect(result).toStricEqual({error: expect.any(String)});
     });
     test(('Description is more than 100 characters in length'), () => {
         let description = [];
@@ -29,7 +30,7 @@ describe(('test_adminQuizDescriptionUpdate'), () => {
             description[i] = 'a';
         }
         let result = adminQuizDescriptionUpdate(data.users[0].authUserId, data.quiezzes[0].quizId, description);
-        expect(result).toStricEqual({error: 'specific error message here'});
+        expect(result).toStricEqual({error: expect.any(String)});
     });
     test(('sucessfulTest1'), () => {
         let result = adminQuizDescriptionUpdate(data.users[0].authUserId, data.quiezzes[0].quizId, 'description_new');
