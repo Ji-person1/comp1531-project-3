9bd709f0e173d3e2f85bec96df689602c5836692
Hao Wu <z5281040@ad.unsw.edu.au>
2024-10-04 17:44:21 +1000

    fixed function 'clear'.-Hao

--- src/other.test.js
+++ src/other.test.js
@@ -13,25 +13,25 @@ describe(('2 tests function "clear"'), () => {
     
     test('2: fill something and clean it', () => {
         const userId = adminAuthRegister('mail@gmail.com', 'aaaaa111', 'firstName', 'lastName');
-        const quizId = adminQuizCreate(userId.id, 'this is name', 'this is description');
+        const quizId = adminQuizCreate(userId.authUserId, 'this is name', 'this is description');
         clear();
         expect({users: [], quizzes: []}).toStrictEqual(getData());
     });
     test('3: fill more things and clean it', () => {
         const userId = adminAuthRegister('mail@gmail.com', 'aaaaa111', 'firstName', 'lastName');
-        const quizId = adminQuizCreate(userId.id, 'this is name', 'this is description');
+        const quizId = adminQuizCreate(userId.authUserId, 'this is name', 'this is description');
         const userId2 = adminAuthRegister('mail2@gmail.com', 'aaaaa111', 'firstName', 'lastName');
-        const quizId2 = adminQuizCreate(userId2.id, 'this is name', 'this is description');
+        const quizId2 = adminQuizCreate(userId2.authUserId, 'this is name', 'this is description');
         clear();
         expect({users: [], quizzes: []}).toStrictEqual(getData());
     });
     test('4: clean and then fill it', () => {
         const userId = adminAuthRegister('mail@gmail.com', 'aaaaa111', 'firstName', 'lastName');
-        const quizId = adminQuizCreate(userId.id, 'this is name', 'this is description');
+        const quizId = adminQuizCreate(userId.authUserId, 'this is name', 'this is description');
         clear();
         const userId2 = adminAuthRegister('mail2@gmail.com', 'aaaaa111', 'firstName', 'lastName');
-        const quizId2 = adminQuizCreate(userId2.id, 'this is name', 'this is description');
-        expect(userId2).toStrictEqual({id: 1});
+        const quizId2 = adminQuizCreate(userId2.authUserId, 'this is name', 'this is description');
+        expect(userId2).toStrictEqual({authUserId: 1});
         expect(quizId2).toStrictEqual({quizId: 1});
     });
 });
