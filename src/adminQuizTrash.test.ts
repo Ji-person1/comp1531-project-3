877406c7e48ab3da57772d908463684703137d83
swapnav saikia <swapnav@LAPTOP-RO9GM7AF>
2024-10-21 17:06:53 +1100

    edited test to check that token is valid for adminQuizTrash.test.ts

--- src/adminQuizTrash.test.ts
+++ src/adminQuizTrash.test.ts
@@ -4,19 +4,17 @@ import { port, url } from './config.json';
 const SERVER_URL = `${url}:${port}`;
 const TIMEOUT_MS = 5 * 1000;
 
-
 const ERROR = { error: expect.any(String) };
-
 beforeEach(() => {
     request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });
-})
+});
 
 describe('Error Cases', () => {
-    let UserToken: {token: string}; 
+    let UserToken: { token: string };
 
     beforeEach(() => {
         const res = request('POST', SERVER_URL + '/v1/admin/auth/register', 
-            {json: {email: "Swapnav.saikia123@icloud.com", password: "1234abcd", nameFirst: "Jim", nameLast: "Zheng"}});
+            { json: { email: "Swapnav.saikia123@icloud.com", password: "1234abcd", nameFirst: "Jim", nameLast: "Zheng" } });
         UserToken = JSON.parse(res.body.toString());
     });
 
@@ -42,14 +40,17 @@ describe('Error Cases', () => {
 });
 
 describe('Success Cases', () => {
-    let UserToken: {token: string}; 
+    let UserToken: { token: string };
+
     beforeEach(() => {
         const res = request('POST', SERVER_URL + '/v1/admin/auth/register', 
-            {json: {email: "Swapnav.saikia123@icloud.com", password: "1234abcd", nameFirst: "Jim", nameLast: "Zheng"}});
+            { json: { email: "Swapnav.saikia123@icloud.com", password: "1234abcd", nameFirst: "Jim", nameLast: "Zheng" } });
         UserToken = JSON.parse(res.body.toString());
     });
 
     test('Valid token', () => {
+        expect(UserToken).toHaveProperty('token');
+        expect(typeof UserToken.token).toBe('string');
 
         const res = request('GET', SERVER_URL + '/v1/admin/quiz/trash', {
             qs: { token: UserToken.token }, 
