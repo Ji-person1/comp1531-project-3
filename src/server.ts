d0f46e8328626797744fe363a2fdb6bf42278ca9
Ji-person1 <139549779+Ji-person1@users.noreply.github.com>
2024-11-16 22:43:04 +1100

    literally more issues with setdata.

--- src/server.ts
+++ src/server.ts
@@ -78,7 +78,7 @@ app.get('/data', async (req: Request, res: Response) => {
 app.put('/data', async (req: Request, res: Response) => {
   try {
     const { data } = req.body;
-    await database.hset('datastore', {datastore: data});
+    await database.hset('datastore', {data});
 
     res.status(200).json(data);
   } catch (error) {
